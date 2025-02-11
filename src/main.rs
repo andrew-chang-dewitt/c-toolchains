b5bf145858f3a4f295a25c28de0936c32d69fc1d
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2025-02-11 06:31:39 -0600

    feat(arch): read host arch as default for output toolchain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+use sysinfo::System;
+
+#[derive(Debug, Parser)]
+#[command(version, about)]
+/// A tool for creating c compiler toolchains for cross compilation using gcc, glibc, binutils, &
+/// the linux kernal
+struct Cli {
+    /// target architecture for the toolchain to be created
+    targetarch: String,
+    /// host architecture for the toolchain to be created
+    #[arg(long)]
+    hostarch: Option<String>,
+    /// build architecture for the toolchain to be created
+    #[arg(long)]
+    buildarch: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cli = Cli::parse();
+    if let None = cli.hostarch {
+        cli.hostarch = Some(System::cpu_arch());
+    }
+    if let None = cli.buildarch {
+        cli.buildarch = Some(System::cpu_arch());
+    }
+
+    println!("cli: {cli:#?}");
 }
